abe6eaa98d2e9bdc4260abce66705c26f14581f9
Marcin Kwiatkowski <m.kwiatkowski@vemco.pl>
2023-09-18 22:53:36 +0200

    new-app looks the same as the old one

---
+++ proxy-routing/app-new/src/app/users/page.tsx
@@ -0,0 +1,8 @@
+export default function Users() {
+  return (
+    <img
+      src='https://upload.wikimedia.org/wikipedia/commons/8/89/ScreenShot-react-mui-datagrid-2.png'
+      alt='Users'
+    />
+  );
+}
